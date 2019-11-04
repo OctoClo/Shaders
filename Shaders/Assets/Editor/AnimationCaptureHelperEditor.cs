4f5d2a7a20043ede7a1ae5667281c16425d79003
Claudia <octoclo@zoho.com>
2019-11-04 16:42:18 +0100

    Dead Cells-like 3D animation capture to 2D

---
+++ Shaders/Assets/Editor/AnimationCaptureHelperEditor.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(AnimationCaptureHelper))]
+public class AnimationCaptureHelperEditor : Editor
+{
+    private const string ASSIGN_REFS_INFO = "Assign the Target and SourceClip to start previewing!";
+
+    private const string LEGACY_ANIM_WARN = "The SourceClip must be marked as Legacy!";
+
+    private const string ASSIGN_CAMERA_INFO = "Assign a camera to start capturing!";
+
+    private IEnumerator _currentCaptureRoutine;
+
+    public override void OnInspectorGUI()
+    {
+        using (new EditorGUI.DisabledScope(_currentCaptureRoutine != null))
+        {
+            var helper = (AnimationCaptureHelper)target;
+            var targetProp = serializedObject.FindProperty("_target");
+            var sourceClipProp = serializedObject.FindProperty("_sourceClip");
+
+            EditorGUILayout.PropertyField(targetProp);
+            EditorGUILayout.PropertyField(sourceClipProp);
+
+            if (targetProp.objectReferenceValue == null
+                || sourceClipProp.objectReferenceValue == null)
+            {
+                EditorGUILayout.HelpBox(ASSIGN_REFS_INFO, MessageType.Info);
+                serializedObject.ApplyModifiedProperties();
+                return;
+            }
+
+            var sourceClip = (AnimationClip)sourceClipProp.objectReferenceValue;
+            if (!sourceClip.legacy)
+            {
+                EditorGUILayout.HelpBox(LEGACY_ANIM_WARN, MessageType.Warning);
+                return;
+            }
+
+            using (new EditorGUILayout.VerticalScope(EditorStyles.helpBox))
+            {
+                EditorGUILayout.LabelField("Animation Options", EditorStyles.boldLabel);
+
+                var fpsProp = serializedObject.FindProperty("_framesPerSecond");
+                EditorGUILayout.PropertyField(fpsProp);
+
+                var previewFrameProp = serializedObject.FindProperty("_currentFrame");
+                var numFrames = (int)(sourceClip.length * fpsProp.intValue);
+
+                using (var changeScope = new EditorGUI.ChangeCheckScope())
+                {
+                    var frame = previewFrameProp.intValue;
+                    frame = EditorGUILayout.IntSlider("Current Frame", frame, 0, numFrames - 1);
+
+                    if (changeScope.changed)
+                    {
+                        previewFrameProp.intValue = frame;
+                        helper.SampleAnimation((frame / (float)numFrames) * sourceClip.length);
+                    }
+                }
+            }
+
+            using (new EditorGUILayout.VerticalScope(EditorStyles.helpBox))
+            {
+                EditorGUILayout.LabelField("Capture Options", EditorStyles.boldLabel);
+
+                var captureCameraProp = serializedObject.FindProperty("_captureCamera");
+                EditorGUILayout.ObjectField(captureCameraProp, typeof(Camera));
+
+                if (captureCameraProp.objectReferenceValue == null)
+                {
+                    EditorGUILayout.HelpBox(ASSIGN_CAMERA_INFO, MessageType.Info);
+                    serializedObject.ApplyModifiedProperties();
+                    return;
+                }
+
+                var resolutionProp = serializedObject.FindProperty("_cellSize");
+                EditorGUILayout.PropertyField(resolutionProp);
+
+                if (GUILayout.Button("Capture"))
+                {
+                    RunRoutine(helper.CaptureAnimation(SaveCapture));
+                }
+            }
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+
+    private void RunRoutine(IEnumerator routine)
+    {
+        _currentCaptureRoutine = routine;
+        EditorApplication.update += UpdateRoutine;
+    }
+
+    private void UpdateRoutine()
+    {
+        if (!_currentCaptureRoutine.MoveNext())
+        {
+            EditorApplication.update -= UpdateRoutine;
+            _currentCaptureRoutine = null;
+        }
+    }
+
+    private void SaveCapture(Texture2D diffuseMap, Texture2D normalMap)
+    {
+        var diffusePath = EditorUtility.SaveFilePanel("Save Capture", "", "NewCapture", "png");
+
+        if (string.IsNullOrEmpty(diffusePath))
+        {
+            return;
+        }
+
+        var fileName = Path.GetFileNameWithoutExtension(diffusePath);
+        var directory = Path.GetDirectoryName(diffusePath);
+        var normalPath = string.Format("{0}/{1}{2}.{3}", directory, fileName, "NormalMap", "png");
+
+        File.WriteAllBytes(diffusePath, diffuseMap.EncodeToPNG());
+        File.WriteAllBytes(normalPath, normalMap.EncodeToPNG());
+
+        AssetDatabase.Refresh();
+    }
+}
+
---
+++ Shaders/Assets/_Project/Scripts/AnimationCaptureHelper.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class AnimationCaptureHelper : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _target = null;
+
+    [SerializeField]
+    private AnimationClip _sourceClip = null;
+
+    [SerializeField]
+    private int _framesPerSecond = 30;
+
+    [SerializeField]
+    private Vector2Int _cellSize = new Vector2Int(100, 100);
+
+    [SerializeField]
+    private int _currentFrame = 0;
+
+    [SerializeField]
+    private Camera _captureCamera = null;
+
+    public void SampleAnimation(float time)
+    {
+        if (_sourceClip == null || _target == null)
+        {
+            Debug.LogWarning("SourceClip and Target should be set before sample animation!");
+            return;
+        }
+        else
+        {
+            _sourceClip.SampleAnimation(_target, time);
+        }
+    }
+
+    public IEnumerator CaptureAnimation(Action<Texture2D, Texture2D> onComplete)
+    {
+        if (_sourceClip == null || _target == null)
+        {
+            Debug.LogWarning("CaptureCamera should be set before capturing animation!");
+            yield break;
+        }
+
+        var numFrames = (int)(_sourceClip.length * _framesPerSecond);
+        var gridCellCount = SqrtCeil(numFrames);
+        var atlasSize = new Vector2Int(_cellSize.x * gridCellCount, _cellSize.y * gridCellCount);
+        var atlasPos = new Vector2Int(0, atlasSize.y - _cellSize.y);
+
+        if (atlasSize.x > 4096 || atlasSize.y > 4096)
+        {
+            Debug.LogErrorFormat("Error attempting to capture an animation with a length and" +
+                "resolution that would produce a texture of size: {0}", atlasSize);
+        }
+
+        var diffuseMap = new Texture2D(atlasSize.x, atlasSize.y, TextureFormat.ARGB32, false)
+        {
+            filterMode = FilterMode.Point
+        };
+        ClearAtlas(diffuseMap, Color.clear);
+
+        var normalMap = new Texture2D(atlasSize.x, atlasSize.y, TextureFormat.ARGB32, false)
+        {
+            filterMode = FilterMode.Point
+        };
+        ClearAtlas(normalMap, new Color(0.5f, 0.5f, 1.0f, 0.0f));
+
+        var rtFrame = new RenderTexture(_cellSize.x, _cellSize.y, 24, RenderTextureFormat.ARGB32)
+        {
+            filterMode = FilterMode.Point,
+            antiAliasing = 1,
+            hideFlags = HideFlags.HideAndDontSave
+        };
+
+        var normalCaptureShader = Shader.Find("Hidden/ViewSpaceNormal");
+
+        _captureCamera.targetTexture = rtFrame;
+        var cachedCameraColor = _captureCamera.backgroundColor;
+
+        try
+        {
+            for (_currentFrame = 0; _currentFrame < numFrames; _currentFrame++)
+            {
+                var currentTime = (_currentFrame / (float)numFrames) * _sourceClip.length;
+                SampleAnimation(currentTime);
+                yield return null;
+
+                _captureCamera.backgroundColor = Color.clear;
+                _captureCamera.Render();
+                Graphics.SetRenderTarget(rtFrame);
+                diffuseMap.ReadPixels(new Rect(0, 0, rtFrame.width, rtFrame.height), atlasPos.x, atlasPos.y);
+                diffuseMap.Apply();
+
+                _captureCamera.backgroundColor = new Color(0.5f, 0.5f, 1.0f, 0.0f);
+                _captureCamera.RenderWithShader(normalCaptureShader, "");
+                Graphics.SetRenderTarget(rtFrame);
+                normalMap.ReadPixels(new Rect(0, 0, rtFrame.width, rtFrame.height), atlasPos.x, atlasPos.y);
+                normalMap.Apply();
+
+                atlasPos.x += _cellSize.x;
+
+                if ((_currentFrame + 1) % gridCellCount == 0)
+                {
+                    atlasPos.x = 0;
+                    atlasPos.y -= _cellSize.y;
+                }
+            }
+            onComplete.Invoke(diffuseMap, normalMap);
+        }
+        finally
+        {
+            Graphics.SetRenderTarget(null);
+            _captureCamera.targetTexture = null;
+            _captureCamera.backgroundColor = cachedCameraColor;
+            DestroyImmediate(rtFrame);
+        }
+    }
+
+    private int SqrtCeil(int input)
+    {
+        return Mathf.CeilToInt(Mathf.Sqrt(input));
+    }
+
+    private void ClearAtlas(Texture2D texture, Color color)
+    {
+        var pixels = new Color[texture.width * texture.height];
+        for (var i = 0; i < pixels.Length; i++)
+        {
+            pixels[i] = color;
+        }
+        texture.SetPixels(pixels);
+        texture.Apply();
+    }
+}
