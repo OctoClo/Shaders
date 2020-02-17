cc03a86f3f72f27ad8bc31b3762f8f5e622dc8dc
Claudia <octoclo@zoho.com>
2020-02-17 15:46:07 +0100

    Heat distortion + flickering light for fire

---
+++ Shaders/Assets/_Project/Scripts/LightFlicker.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightFlicker : MonoBehaviour
+{
+    Light currentLight;
+    public float minIntensity = 2f;
+    public float maxIntensity = 4f;
+
+    float random;
+
+    void Start()
+    {
+        currentLight = GetComponent<Light>();
+        random = Random.Range(0.0f, 65535.0f);
+    }
+
+    void Update()
+    {
+        float noise = Mathf.PerlinNoise(random, Time.time * 3);
+        currentLight.intensity = Mathf.Lerp(minIntensity, maxIntensity, noise);
+    }
+}
+
